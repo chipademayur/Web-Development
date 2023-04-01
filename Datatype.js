25bc4da5ac324812540cf996abec77527770409c
chipademayur <101926125+chipademayur@users.noreply.github.com>
2023-04-01 10:24:39 +0530

    Add files via upload

--- Datatype.js
+++ Datatype.js
@@ -1,4 +1,33 @@
- alert("hi");
+alert("hi");
 //  Numbers
-let day=5;
-console.log(typeof day);
+// let day=5;
+// console.log(typeof day);
+// concatination
+
+//  let bank1=100;
+//  let bank2=200;
+
+// let balance=bank1+bank2;
+// console.log(balance);
+
+// String Concatination
+const greet1 = "Welcome mayur";
+const user = "hi";
+console.log(greet1 + user);
+
+let mayur = "hmaia";
+
+// conditional
+// (<,>,=,==,=>,<=)
+// const access = false;
+
+// if (access) {
+//   console.log("hi i am a vetry mad student ");
+// } else {
+//   alert("hey vedya");
+// }
+
+let a = 10;
+if (a > 10) {
+  alert("you are small");
+}
